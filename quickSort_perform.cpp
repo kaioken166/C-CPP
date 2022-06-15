8b99f1fb478d306dfe70f6d50b56685b14a7d13a
Tri <trihuynh02012002@gmail.com>
2022-06-15 10:35:59 +0700

    update

--- quickSort_perform.cpp
+++ quickSort_perform.cpp
@@ -32,6 +32,30 @@ void quickSort(int arr[], int first, int last)
         quickSort(arr, upper + 1, last);
 }
 
+void quickSort1(int a[], int first, int last)
+{
+    int lower = first + 1, upper = last - 1;
+    swap(a[first], a[(first + last) / 2]);
+    int bound = a[first];
+
+    while (lower <= upper)
+    {
+        while (bound > a[lower])
+            lower++;
+        while (bound < a[upper])
+            upper--;
+        if (lower < upper)
+            swap(a[lower++], a[upper--]);
+        else
+            lower++;
+    }
+    swap(a[upper], a[first]);
+    if (first < upper - 1)
+        quickSort1(a, first, upper - 1);
+    if (upper + 1 < last)
+        quickSort1(a, upper + 1, last);
+}
+
 int main()
 {
     int n = 10;
